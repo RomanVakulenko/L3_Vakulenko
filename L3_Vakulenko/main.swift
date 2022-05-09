91870f9a602b4d7ba85b0d47234c7e1df2dae63a
RomanVakulenko <vakula_48@mail.ru>
2022-05-09 14:42:57 +0300

    Исправил ошибку

--- L3_Vakulenko/main.swift
+++ L3_Vakulenko/main.swift
@@ -13,10 +13,10 @@ struct SportCar {
     let turnEngineOff: Bool
     let openWindows: Bool
     
-    mutating func change (chanhingTrunkVolume: ActionWithCar, cargoAmount: Int){
-        switch chanhingTrunkVolume {
+    mutating func change (changingTrunkVolume: ActionWithCar, cargoAmount: Int){
+        switch changingTrunkVolume {
         case .putCargoInCar:
-           print ("TrunkVolume became = \(trunkVolume = trunkVolume - cargoAmount)")
+           print ("Not used trunkVolume of SportCar became = \(trunkVolume - cargoAmount)")
         default:
             print("Cargo was't putted in SportCar")
         }
@@ -30,22 +30,18 @@ struct TrunkCar {
     let turnEngineOff: Bool
     let openWindows: Bool
 }
-// 5. Инициализировать несколько экземпляров структур.
-var audiRS6 = SportCar (brand: "audi", productionYear: 2020, trunkVolume: 900, turnEngineOn: true, turnEngineOff: false, openWindows: false)
-var maz = TrunkCar (brand: "MAZ", productionYear: 2009, trunkVolume: 8000, turnEngineOn: false, turnEngineOff: true, openWindows: true)
 // 3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
 enum ActionWithCar {
     case turnEngineOn, turnEngineOff
     case openWindows, closeWindows
     case putCargoInCar, takeOffCargoFromCar
 }
-
 // 4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
-SportCar.change(<#T##self: &SportCar##SportCar#>)
-
 // 5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
-audiRS6.trunkVolume = 300
-maz.trunkVolume = 7000
+var audiRS6 = SportCar (brand: "audi", productionYear: 2020, trunkVolume: 900, turnEngineOn: true, turnEngineOff: false, openWindows: false) //- здесь я инициализировал экземпляр, так?
+audiRS6.change(changingTrunkVolume: .putCargoInCar, cargoAmount: 100) // а тут я уже применил действие к экземпляру
+var maz = TrunkCar (brand: "MAZ", productionYear: 2009, trunkVolume: 8000, turnEngineOn: false, turnEngineOff: true, openWindows: true)
+maz.trunkVolume = 7000  // тут как я понимаю я НЕ инициализировал - верно я понял?
 
 // 6. Вывести значения свойств экземпляров в консоль.
 print("марка машины - \(audiRS6.brand)\n", "год выпуска - \(audiRS6.productionYear)\n", "объем багажника = \(audiRS6.trunkVolume)\n", "запущен ли двигатель - \(audiRS6.turnEngineOn)")
